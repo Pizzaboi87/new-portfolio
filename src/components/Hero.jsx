102d24d71355fa8bcfc212bbf95358bfe36ea2fe
Peter Weiser <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-29 09:27:49 +0300

    added useInView to canvas div and minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import { motion } from "framer-motion";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import { styles } from "../styles";
 import { AtAtCanvas } from "./canvas";
 
 const Hero = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -22,20 +26,23 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="md:w-[80%] md:h-full md:mt-12 right-0 mt-28 w-full h-[80%] absolute">
-        <AtAtCanvas />
+      <div
+        ref={ref}
+        className="md:w-[80%] md:h-full md:mt-12 right-0 mt-28 w-full h-[80%] absolute"
+      >
+        {isInView && <AtAtCanvas />}
       </div>
       <div className="absolute xs:bottom-10 bottom-32 w-full pr-6 flex justify-end items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-orange flex justify-center items-start p-2">
-            <motion.div 
+            <motion.div
               animate={{
-                y: [0, 24, 0]
+                y: [0, 24, 0],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: 'loop'
+                repeatType: "loop",
               }}
               className="w-3 h-3 rounded-full bg-orange mb-1"
             />

035a6f40946aaf5e6e53f39f55842aeed8799f41
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-29 11:20:24 +0300

    changed layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
-import { useRef } from "react";
-import { motion, useInView } from "framer-motion";
-import { styles } from "../styles";
-import { AtAtCanvas } from "./canvas";
+import { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
+import { styles } from '../styles';
+import { AtAtCanvas } from './canvas';
 
 const Hero = () => {
   const ref = useRef(null);
   const isInView = useInView(ref);
 
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto grid">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -26,11 +26,14 @@ const Hero = () => {
           </p>
         </div>
       </div>
+
       <div
         ref={ref}
-        className="md:w-[80%] md:h-full md:mt-12 right-0 mt-28 w-full h-[80%] absolute"
+        className="md:w-[80%] justify-self-end md:h-full md:mt-12 right-0 mt-28 w-full h-[80%] relative overflow-hidden"
       >
-        {isInView && <AtAtCanvas />}
+        <div className="overflow-auto relative w-full h-full">
+          {isInView && <AtAtCanvas />}
+        </div>
       </div>
       <div className="absolute xs:bottom-10 bottom-32 w-full pr-6 flex justify-end items-center">
         <a href="#about">
@@ -42,7 +45,7 @@ const Hero = () => {
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: "loop",
+                repeatType: 'loop',
               }}
               className="w-3 h-3 rounded-full bg-orange mb-1"
             />
--- src/components/canvas/AtAt.jsx
+++ src/components/canvas/AtAt.jsx
@@ -1,8 +1,8 @@
-import Loader from "../Loader";
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload } from "@react-three/drei";
-import { Model } from "./AtAtScene";
+import Loader from '../Loader';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload } from '@react-three/drei';
+import { Model } from './AtAtScene';
 
 const AtAtCanvas = () => {
   return (
@@ -21,6 +21,7 @@ const AtAtCanvas = () => {
       />
       <Suspense fallback={<Loader />}>
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}

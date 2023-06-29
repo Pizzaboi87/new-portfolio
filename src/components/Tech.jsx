e1806a8d74ec1c5ed9f9e700ab1eb9ea662a56bb
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-29 10:04:41 +0300

    modified tech icons carousel

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,12 +1,16 @@
-import { useMemo } from "react";
-import { techLogos } from "../constants";
-import { Icon } from "@iconify/react";
+import { useMemo } from 'react';
+import { techLogos } from '../constants';
+import { Icon } from '@iconify/react';
 
 const Tech = () => {
   const memoizedIcons = useMemo(
     () =>
       techLogos.map((logo, index) => (
-        <li className="brand-logo" key={`${logo}-${index}`}>
+        <li
+          className="md:text-5xl text-4xl text-center"
+          style={{ width: 'var(--logo-width)' }}
+          key={`${logo}-${index}`}
+        >
           <Icon icon={logo} />
         </li>
       )),
@@ -14,8 +18,11 @@ const Tech = () => {
   );
 
   return (
-    <div className="slider">
-      <ul className="brands">
+    <div className="slider relative w-[100vw] md:h-24 h-16 bg-white flex items-center overflow-hidden">
+      <ul
+        className="brands flex list-none"
+        style={{ width: 'var(--total-logo-width)' }}
+      >
         {memoizedIcons}
         {memoizedIcons}
       </ul>

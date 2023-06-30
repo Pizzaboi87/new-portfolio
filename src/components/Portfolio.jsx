8b9b986d76eeb164556f4bd1de25990a495c17bd
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-30 20:57:58 +0300

    created mobile project cards

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,61 @@
+import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
 import { fadeIn, textVariant } from '../utils/motion';
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
+import { Icon } from '@iconify/react';
 
-const ProjectCard = ({ name }) => {
-  return <p>{name}</p>;
+const MobileProjectCard = ({
+  index,
+  id,
+  title,
+  alt,
+  link,
+  github,
+  cover,
+  youTube,
+  details,
+}) => {
+  return (
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      <Tilt
+        max={45}
+        scale={1}
+        speed={450}
+        glareEnable
+        glarePosition="bottom"
+        glareColor="#ffffff"
+        className="bg-tertiary p-5 sm:w-[360px] w-full min-h-[530px]"
+      >
+        <div className="relative w-full h-[230px]">
+          <img src={cover} alt={alt} className="w-full h-full object-cover" />
+          <div className="absolute inset-0 flex justify-start m-3">
+            <div
+              onClick={() => window.open(link, '_blank')}
+              className="yellow-purple-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-10"
+            >
+              <Icon icon="vaadin:eye" style={{ fontSize: '2rem' }} />
+            </div>
+          </div>
+          <div className="absolute inset-0 flex justify-end m-3">
+            <div
+              onClick={() => window.open(github, '_blank')}
+              className="yellow-purple-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-10"
+            >
+              <Icon icon="mdi:github" style={{ fontSize: '2rem' }} />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{title}</h3>
+          <p className="mt-2 text-secondary text-[14px] text-justify">
+            {details}
+          </p>
+        </div>
+      </Tilt>
+    </motion.div>
+  );
 };
 
 const Portfolio = () => {
@@ -31,9 +81,9 @@ const Portfolio = () => {
         </motion.div>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-6 justify-center">
+      <div className="mt-20 flex flex-wrap gap-7 justify-center">
         {projects.map((project, index) => (
-          <ProjectCard key={`${project.name}-${index}`} {...project} />
+          <MobileProjectCard key={project.id} {...project} index={index} />
         ))}
       </div>
     </>
